ac080db8766612f68c9d80806dbca03a7dccd9ec
zahin-beta <zahin69ff@gmail.com>
2026-03-08 20:51:42 +0600

    window updated

--- src/main.c
+++ src/main.c
@@ -5,7 +5,20 @@
 #include <stdlib.h>
 #include <math.h>
 
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
 int main ()
 {
-    
+    int sw = 480, sh = 480;
+    InitWindow(sw,sh,"SNAKE GAME");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(green);
+        EndDrawing();
+    }
+    CloseWindow();
 }
