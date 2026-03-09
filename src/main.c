dcb2a71e147d06f25e2b0b4a6340243602a26343
zahin-beta <zahin69ff@gmail.com>
2026-03-09 22:14:20 +0600

    created food

--- src/main.c
+++ src/main.c
@@ -1,28 +1,57 @@
-//snake game remake in c
+// snake game remake in c
 //@author: Zahin Bin Hasan
 #include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+#define CELL_SIZE 30
+#define CELL_COUNT 25
+
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
 
-int main ()
+typedef struct Food
 {
-    int sw = 480, sh = 480;
-    InitWindow(sw,sh,"SNAKE GAME");
+    Vector2 pos;
+    Texture2D texture;
+} Food;
+
+void foodDraw(Food *food)
+{
+    DrawTexture(food->texture, food->pos.x*CELL_SIZE,food->pos.y*CELL_SIZE, WHITE);
+}
+
+Vector2 randomPos ()
+{
+    int x = GetRandomValue(0, CELL_COUNT - 1);
+    int y = GetRandomValue(0, CELL_COUNT - 1);
+    return (Vector2){x,y};
+}
+
+Food createFood ()
+{
+    Food f =
+        {
+            .pos = randomPos(),
+            .texture = LoadTexture("../assets/food.png")
+        };
+    return f;
+}
+
+int main()
+{
+    InitWindow(CELL_SIZE * CELL_COUNT, CELL_SIZE * CELL_COUNT, "SNAKE GAME");
     SetTargetFPS(60);
 
-    Texture2D food = LoadTexture("../assets/food.png");
-
+    Food food = createFood();
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(green);
-        DrawTexture(food,100,100,WHITE);
+        foodDraw(&food);
         EndDrawing();
     }
-    UnloadTexture(food);
+    UnloadTexture(food.texture);
     CloseWindow();
 }
