ca8e9f4a261481037558254ec55c97f3f6f57e90
zahin-beta <zahin69ff@gmail.com>
2026-03-10 02:57:34 +0600

    trial stage

--- src/main.c
+++ src/main.c
@@ -53,6 +53,7 @@ void initialSnake(Snake *s)
     s->body[0] = (Vector2){CELL_COUNT / 2, CELL_COUNT / 2};
     s->body[1] = (Vector2){CELL_COUNT / 2 - 1, CELL_COUNT / 2};
     s->body[2] = (Vector2){CELL_COUNT / 2 - 2, CELL_COUNT / 2};
+    s->direction = (Vector2){1, 0};
 }
 
 void drawSnake(Snake *s)
@@ -64,19 +65,19 @@ void drawSnake(Snake *s)
 }
 void input(Snake *s)
 {
-    if (IsKeyPressed(KEY_W))
+    if (IsKeyPressed(KEY_W) && s->direction.y != 1)
     {
         s->direction = (Vector2){0, -1};
     }
-    if (IsKeyPressed(KEY_S))
+    if (IsKeyPressed(KEY_S) && s->direction.y != -1)
     {
         s->direction = (Vector2){0, 1};
     }
-    if (IsKeyPressed(KEY_A))
+    if (IsKeyPressed(KEY_A) && s->direction.x != 1)
     {
-        s->direction = (Vector2){-1, 0} ;
+        s->direction = (Vector2){-1, 0};
     }
-    if (IsKeyPressed(KEY_D))
+    if (IsKeyPressed(KEY_D) && s->direction.x != -1)
     {
         s->direction = (Vector2){1, 0};
     }
@@ -88,33 +89,102 @@ void snakeMovement(Snake *s)
     {
         s->body[i] = s->body[i - 1];
     }
-    s->body[0].x += s->direction.x;
-    s->body[0].y += s->direction.y;
+    {
+        s->body[0].x += s->direction.x;
+        s->body[0].y += s->direction.y;
+    }
 }
 
 // ending snake struct
+
+// collision
+
+bool checkSelfCollision(Snake *s)
+{
+    for (size_t i = 1; i < s->length; i++)
+    {
+        if (s->body[0].x == s->body[i].x && s->body[0].y == s->body[i].y)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool wallCollision(Snake *s)
+{
+    if (s->body[0].x < 0 || s->body[0].x >= CELL_COUNT || s->body[0].y < 0 || s->body[0].y >= CELL_COUNT)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool checkCollision(Snake *s, Food *f)
+{
+    if (s->body[0].x == f->pos.x && s->body[0].y == f->pos.y)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+//
+// text
+void drawScore(int *score)
+{
+    DrawText(TextFormat("SCORE: %d", *score), 20, 20, 30, BLACK);
+}
+
+//
 
 int main()
 {
     InitWindow(CELL_SIZE * CELL_COUNT, CELL_SIZE * CELL_COUNT, "SNAKE GAME");
     SetTargetFPS(60);
     int framecount = 0;
+    int score = 0;
     Food food = createFood();
     Snake snake;
     initialSnake(&snake);
+    bool gameOver = false;
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        framecount++;
-        input(&snake);
-        if (framecount == 10)
+        if (!gameOver)
         {
-            snakeMovement(&snake);
-            framecount = 0;
+            framecount++;
+            input(&snake);
+            if (framecount == 10)
+            {
+                snakeMovement(&snake);
+                framecount = 0;
+            }
+            ClearBackground(green);
+            foodDraw(&food);
+            drawSnake(&snake);
+            drawScore(&score);
+            if (checkCollision(&snake, &food))
+            {
+                score++;
+
+                food.pos = randomPos();
+                snake.length++;
+            }
+            if (checkSelfCollision(&snake) || wallCollision(&snake))
+            {
+                gameOver = true;
+            }
         }
-        ClearBackground(green);
-        foodDraw(&food);
-        drawSnake(&snake);
+        else
+        {
+            DrawText("GAME OVER", 300, 350, 40, BLACK);
+            DrawText(TextFormat("SCORE: %d", score), 300, 350 + 50, 40, BLACK);
+        }
+        
+
         EndDrawing();
     }
     UnloadTexture(food.texture);
